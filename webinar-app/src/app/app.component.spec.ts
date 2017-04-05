5b4757a111abfac83361311df7dea008ddb8e2c6
bonnichiwa <bonnie.pham@ryerson.ca>
2017-04-05 14:28:00 -0400

    added more tests

--- webinar-app/src/app/app.component.spec.ts
+++ webinar-app/src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TodoService } from './services/todo.service';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let fakeTodoService = {
+    addTask: jasmine.createSpy('addTask')
+  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: TodoService, useValue: fakeTodoService}
+        ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -20,13 +28,21 @@ describe('AppComponent', () => {
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('TODO List Application');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('TODO List Application');
   }));
+
+  it('should call addTask in the service', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.addTask('hello');
+    expect(fakeTodoService.addTask).toHaveBeenCalledTimes(1);
+    expect(fakeTodoService.addTask).toHaveBeenCalledWith('hello');
+  });
 });
